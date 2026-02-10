01d45d51297759ad8f04c51b420557a5c74876c9
chaliepark <gofl3434@gmail.com>
2026-02-10 13:31:11 +0900

    각 작업공간 생

--- modu/20260210.py
+++ modu/20260210.py
@@ -3,7 +3,16 @@ Created on 2026. 1. 26.
 
 @author: chaliepark
 '''
-import numpy as np
+from kimchangil import kimchangil_20260210
+from chakyunghwan import chakyunghwan_20260210
+from shingiju import shingiju_20260210
+from parkbyungin import parkbyungin_20260210
+from parkjinchoel import parkjinchoel_20260210
 
-for t in np.arange(10):
-    print(t)
+
+print(kimchangil_20260210.author_name)
+print(chakyunghwan_20260210.author_name)
+print(shingiju_20260210.author_name)
+print(parkbyungin_20260210.author_name)
+print(parkjinchoel_20260210.author_name)
+
